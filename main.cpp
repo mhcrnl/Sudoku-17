ec5ac5734ee6ff9a77b0b78ba4e7b711a8c91ac5
pravind <pravind.2k4@gmail.com>
2012-12-11 08:08:45 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "puzzleSolver.h"
+
+int main()
+{
+  SudokuGrid sudoku;
+	//PuzzleSolver solver(PUZZLE_TYPE::sudoku);
+	PuzzleSolver& solver = PuzzleSolver::getPuzzleSolver(PUZZLE_TYPE::sudoku);
+	solver.solve(sudoku);
+	return 0;
+}
